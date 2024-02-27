e67e709946edbd6a0b6a1adccb17bd9f2ec8331f
Zarrin Rahman <zarrin_r01@hotmail.co.uk>
2024-02-27 16:39:26 +0000

    created getMovies method to get all movies in database

--- src/main/java/com/example/cinema/services/MovieService.java
+++ src/main/java/com/example/cinema/services/MovieService.java
@@ -1,11 +1,12 @@
 package com.example.cinema.services;
 
 import com.example.cinema.models.Movie;
-import com.example.cinema.models.Rating;
+
 import com.example.cinema.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -24,6 +25,10 @@ public class MovieService {
 
     public Optional<Movie> getMovieById(long id) {
         return movieRepository.findById(id);
+    }
+
+    public List<Movie> getMovies() {
+        return movieRepository.findAll();
     }
 
 }
